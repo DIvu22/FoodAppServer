df385fe361f72b9a64cededbf3791dad39b16f8c
DIvu22 <divu1666@gmail.com>
2018-04-29 13:38:02 +0530

    Update and delete food item feature implemented

--- app/src/main/java/serverside/example/com/serverside/FoodList.java
+++ app/src/main/java/serverside/example/com/serverside/FoodList.java
@@ -13,6 +13,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RelativeLayout;
@@ -241,6 +242,134 @@ Uri saveUri;
         {
             saveUri=data.getData();
             btn_select.setText("Image Selected");
+        }
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+
+        if(item.getTitle().equals(Common.UPDATE))
+        {
+            showUpdateFoodDialog(adapter.getRef(item.getOrder()).getKey(),adapter.getItem(item.getOrder()));
+        }
+
+        else if(item.getTitle().equals(Common.DELETE))
+        {
+                deleteFood(adapter.getRef(item.getOrder()).getKey());
+        }
+        return super.onContextItemSelected(item);
+    }
+
+    private void deleteFood(String key) {
+        FoodList.child(key).removeValue();
+    }
+
+    private void showUpdateFoodDialog(final String key, final Food item) {
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(FoodList.this);
+        alertDialog.setTitle("Edit Food Item");
+        alertDialog.setMessage("Please fill full information");
+        LayoutInflater inflater=this.getLayoutInflater();
+        View add_menu_layout=inflater.inflate(R.layout.add_new_food_layout,null);
+
+        edtName=(MaterialEditText) add_menu_layout.findViewById(R.id.edtName);
+        edtDescription=(MaterialEditText) add_menu_layout.findViewById(R.id.edtDescription);
+        edtPrice=(MaterialEditText) add_menu_layout.findViewById(R.id.edtPrice);
+        edtDiscount=(MaterialEditText) add_menu_layout.findViewById(R.id.edtDiscount);
+
+        edtName.setText(item.getName());
+        edtDescription.setText(item.getDescription());
+        edtPrice.setText(item.getPrice());
+        edtDiscount.setText(item.getDiscount());
+
+        btn_upload=(Button)add_menu_layout.findViewById(R.id.btn_Upload);
+        btn_select=(Button)add_menu_layout.findViewById(R.id.btn_Select);
+
+        btn_select.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                chooseImage();
+            }
+        });
+
+        btn_upload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                changeImage(item);
+            }
+        });
+
+
+        alertDialog.setView(add_menu_layout);
+        alertDialog.setIcon(R.drawable.ic_shopping_cart_black_24dp);
+
+        alertDialog.setPositiveButton("YES", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+
+
+                    item.setName(edtName.getText().toString());
+                    item.setPrice(edtPrice.getText().toString());
+                    item.setDescription(edtDescription.getText().toString());
+                    item.setDiscount(edtDiscount.getText().toString());
+
+                    FoodList.child(key).setValue(item);
+                    Snackbar.make(rootLayout,"Category edited",Snackbar.LENGTH_SHORT).show();
+
+
+            }
+        });
+
+        alertDialog.setNegativeButton("NO", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+
+            }
+        });
+        alertDialog.show();
+
+    }
+
+    private void changeImage(final Food item) {
+        if(saveUri!=null)
+        {
+            final ProgressDialog mDialog=new ProgressDialog(this);
+            mDialog.setMessage("Uploading...");
+            mDialog.show();
+
+            String imageName= UUID.randomUUID().toString();
+            final StorageReference imageFolder=storageReference.child("images/"+imageName);
+            imageFolder.putFile(saveUri)
+                    .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                            mDialog.dismiss();
+                            Toast.makeText(FoodList.this,"Uploaded!!",Toast.LENGTH_SHORT).show();
+                            imageFolder.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                                @Override
+                                public void onSuccess(Uri uri) {
+                                    item.setImage(uri.toString());
+                                }
+                            });
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            mDialog.dismiss();
+                            Toast.makeText(FoodList.this,""+e.getMessage(),Toast.LENGTH_SHORT).show();
+
+                        }
+                    })
+                    .addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
+
+                            double progress=(100.0* taskSnapshot.getBytesTransferred()/taskSnapshot.getTotalByteCount());
+                            mDialog.setMessage("Uploaded!!"+progress+"%");
+                        }
+                    });
         }
     }
 }
